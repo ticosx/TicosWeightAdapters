9cce7af865a754769207bb3937d91d127b96e671
ligping <543227995@qq.com>
2022-07-27 16:37:06 +0800

    refactor: 优化weight adapter 代码

--- src/TkWgHx711.cpp
+++ src/TkWgHx711.cpp
@@ -1,75 +1,71 @@
-
 #include "TkWgHx711.h"
+#include "Log.h"
 
 #define SHIFTIN_WITH_SPEED_SUPPORT(data,clock,order) shiftIn(data,clock,order)
-
-// 
+//
 #define CLK_MODE   OUTPUT
 #define DOUT_MODE  INPUT_PULLUP
 #define    POWERON            0
 #define    POWEROFF           1
 
-TkHx711::TkHx711(weight_info *info) : WeightAdapter(info) {
+TkWgHx711::TkWgHx711(weight_info *info) : WeightAdapter(info) {
 
 }
 
-void TkHx711::power_down() {
-
-    digitalWrite(PWIO, POWEROFF);
+void TkWgHx711::power_down() {
+	digitalWrite(pwio, POWEROFF);
 }
 
-void TkHx711::power_up() {
-	digitalWrite(PWIO, POWERON);
+void TkWgHx711::power_up() {
+	digitalWrite(pwio, POWERON);
 }
 
 
-void TkHx711::begin() {
-
-	pinMode(SCK, CLK_MODE);
-	pinMode(DOUT, DOUT_MODE);
-    pinMode(PWIO, OUTPUT);
-    power_up() ;
-	setGain(GAIN);
+void TkWgHx711::begin() {
+	pinMode(sck, CLK_MODE);
+	pinMode(dout, DOUT_MODE);
+	pinMode(pwio, OUTPUT);
+	power_up() ;
+	setGain(gain);
 }
 
-void TkHx711::end() {
-    power_down() ;
+void TkWgHx711::end() {
+	power_down() ;
 }
 
-bool TkHx711::init() {
+bool TkWgHx711::init() {
+	dout = info->dout_pin;
+	sck = info->sck_pin;
+	pwio  = info->pw_pin;
+	gain = info->gain;
+	begin();
+	logInfo("WgHx711_init ,GAIN = %d,DOUT= %d,PD_SCK= %d,PWIO= %d\n",gain,dout,sck,pwio);
 
-    DOUT = info->dout_pin;
-    SCK = info->sck_pin;
-    PWIO  = info->pw_pin;
-    GAIN = info->gain;
-    begin();
-    printf("WgHx711_init ,GAIN = %d,DOUT= %d,PD_SCK= %d,PWIO= %d\n",GAIN,DOUT,SCK,PWIO);
-
-    return true;
+	return true;
 }
-bool TkHx711::deinit() {    
-    end();
-    printf("weight deinit \n");
-    return true;
+bool TkWgHx711::deinit() {    
+	end();
+	logInfo("weight deinit \n");
+	return true;
 }
-bool TkHx711::isReady() {
-	return digitalRead(DOUT) == 0;
+bool TkWgHx711::isReady() {
+	return digitalRead(dout) == 0;
 }
 
-void TkHx711::setGain(uint8_t gain) {
-	switch (gain) {
+void TkWgHx711::setGain(uint8_t gainValue) {
+	switch (gainValue) {
 		case 128:      // channel A, gain factor 128
-			GAIN = 1;
+			gain = 1;
 			break;
 		case 64:      // channel A, gain factor 64
-			GAIN = 3;
+			gain = 3;
 			break;
 		case 32:      // channel B, gain factor 32
-			GAIN = 2;
+			gain = 2;
 			break;
-	}
+		}
 }
-void TkHx711::waitReady(uint32_t delay_ms) {
+void TkWgHx711::waitReady(uint32_t delay_ms) {
 	// Wait for the chip to become ready.
 	// This is a blocking implementation and will
 	// halt the sketch until a load cell is connected.
@@ -79,10 +75,9 @@ void TkHx711::waitReady(uint32_t delay_ms) {
 	}
 }
 
-bool TkHx711::waitReadyRetry(int retries, uint32_t delay_ms) {
+bool TkWgHx711::waitReadyRetry(int retries, uint32_t delay_ms) {
 	// Wait for the chip to become ready by
 	// retrying for a specified amount of attempts.
-
 	int count = 0;
 	while (count < retries) {
 		if (isReady()) {
@@ -94,7 +89,7 @@ bool TkHx711::waitReadyRetry(int retries, uint32_t delay_ms) {
 	return false;
 }
 
-bool TkHx711::waitReadyTimeout(uint32_t timeout, uint32_t delay_ms) {
+bool TkWgHx711::waitReadyTimeout(uint32_t timeout, uint32_t delay_ms) {
 	// Wait for the chip to become ready until timeout.
 	unsigned long millisStarted = millis();
 	while (millis() - millisStarted < timeout) {
@@ -106,38 +101,34 @@ bool TkHx711::waitReadyTimeout(uint32_t timeout, uint32_t delay_ms) {
 	return false;
 }
 
-uint32_t TkHx711::read() {
+uint32_t TkWgHx711::read() {
 
 	// Wait for the chip to become ready.
 	waitReady(10);
-
 	// Define structures for reading data into.
 	uint32_t value = 0;
 	uint8_t data[3] = { 0 };
 	uint8_t filler = 0x00;
-
 	// Disable interrupts.
 	noInterrupts();
-
 	// Pulse the clock pin 24 times to read the data.
-	data[2] = SHIFTIN_WITH_SPEED_SUPPORT(DOUT, SCK, MSBFIRST);
-	data[1] = SHIFTIN_WITH_SPEED_SUPPORT(DOUT, SCK, MSBFIRST);
-	data[0] = SHIFTIN_WITH_SPEED_SUPPORT(DOUT, SCK, MSBFIRST);
+	data[2] = SHIFTIN_WITH_SPEED_SUPPORT(dout, sck, MSBFIRST);
+	data[1] = SHIFTIN_WITH_SPEED_SUPPORT(dout, sck, MSBFIRST);
+	data[0] = SHIFTIN_WITH_SPEED_SUPPORT(dout, sck, MSBFIRST);
 
 	// Set the channel and the gain factor for the next reading using the clock pin.
-	for (uint8_t i = 0; i < GAIN; i++) {
-		digitalWrite(SCK, HIGH);
-		digitalWrite(SCK, LOW);	
+	for (uint8_t i = 0; i < gain; i++) {
+		digitalWrite(sck, HIGH);
+		digitalWrite(sck, LOW);	
 	}
-    // Enable interrupts again.
-    interrupts()
-    // Replicate the most significant bit to pad out a 32-bit signed integer
+	// Enable interrupts again.
+	interrupts()
+	// Replicate the most significant bit to pad out a 32-bit signed integer
 	if (data[2] & 0x80) {
 		filler = 0xFF;
 	} else {
 		filler = 0x00;
 	}
-
 	// Construct a 32-bit signed integer
 	value = ( static_cast<uint32_t>(filler) << 24
 			| static_cast<uint32_t>(data[2]) << 16
@@ -147,45 +138,42 @@ uint32_t TkHx711::read() {
 	return static_cast<uint32_t>(value);
 }
 
-
-
-uint32_t TkHx711::readAverage(uint32_t times) {
+uint32_t TkWgHx711::readAverage(uint32_t times) {
 	uint32_t sum = 0;
 	for (uint32_t i = 0; i < times; i++) {
 		sum += read();
-
 		delay(0);
 	}
 	return sum / times;
 }
 
-uint32_t TkHx711::getValue(uint32_t times) {
-	return readAverage(times) - OFFSET;
+uint32_t TkWgHx711::getValue(uint32_t times) {
+	return readAverage(times) - offset_value;
 }
 
-float TkHx711::getUnits(uint32_t times) {
-	return getValue(times) / SCALE;
+float TkWgHx711::getUnits(uint32_t times) {
+	return getValue(times) / scale_value;
 }
 
-void TkHx711::tare(uint32_t times) {
+void TkWgHx711::tare(uint32_t times) {
 	uint32_t sum = readAverage(times);
 	setOffset(sum);
 }
 
-void TkHx711::setScale(float scale) {
-	SCALE = scale;
+void TkWgHx711::setScale(float scale) {
+	scale_value = scale;
 }
 
-float TkHx711::getScale() {
-	return SCALE;
+float TkWgHx711::getScale() {
+	return scale_value;
 }
 
-void TkHx711::setOffset(uint32_t offset) {
-	OFFSET = offset;
+void TkWgHx711::setOffset(uint32_t offset) {
+	offset_value = offset;
 }
 
-uint32_t TkHx711::getOffset() {
-	return OFFSET;
+uint32_t TkWgHx711::getOffset() {
+	return offset_value;
 }
 
 
--- src/TkWgHx711.h
+++ src/TkWgHx711.h
@@ -3,14 +3,13 @@
 
 #include <Arduino.h>
 #include "WeightAdapter.h"
-
 /*!
  * @brief The class of hx711
  */
-class TkHx711: public WeightAdapter{
+class TkWgHx711: public WeightAdapter{
 
     public:
-        TkHx711(weight_info *info);
+        TkWgHx711(weight_info *info);
         // get the weight value
         virtual uint32_t getValue(uint32_t times) override ;
         // set OFFSET, the value that's subtracted from the actual reading (tare weight)
@@ -21,7 +20,6 @@ class TkHx711: public WeightAdapter{
         virtual void setScale(float scale) override ;
         // get the current SCALE
         virtual  float getScale() override ;
-           
         /*!
         *    @brief  初始化设备
         *    @return 初始化成功则返回 true
@@ -37,32 +35,32 @@ class TkHx711: public WeightAdapter{
         // set the gain factor; takes effect only after a call to read()
         // channel A can be set for a 128 or 64 gain; channel B has a fixed 32 gain
         // depending on the parameter, the channel is also set to either A or B
-        void setGain(uint8_t gain)  ;
+        void setGain(uint8_t gainValue);
         // waits for the chip to be ready and returns a reading
-        uint32_t read() ;
+        uint32_t read();
         // returns an average reading; times = how many times to read
-        uint32_t readAverage(uint32_t times)  ;
+        uint32_t readAverage(uint32_t times);
         // returns get_value() divided by SCALE, that is the raw value divided by a value obtained via calibration
-        float getUnits(uint32_t times)  ;
+        float getUnits(uint32_t times);
         // set the OFFSET value for tare weight; times = how many times to read the tare value
-        void tare(uint32_t times)  ;
+        void tare(uint32_t times);
         // Wait for the HX711 to become ready
-        bool isReady() ;
-        void waitReady(uint32_t delay_ms ) ;
+        bool isReady();
+        void waitReady(uint32_t delay_ms );
         bool waitReadyRetry(int retries , uint32_t delay_ms);
-        bool waitReadyTimeout(uint32_t timeout , uint32_t delay_ms)  ;
+        bool waitReadyTimeout(uint32_t timeout , uint32_t delay_ms);
         //hx711 init and deinit 
         void begin();
-        void end() ;
+        void end();
         void power_down();
         void power_up();
 
-        uint8_t  SCK; // Power Down and Serial Clock Input Pin
-        uint8_t  DOUT; // Serial Data Output Pin
-        uint8_t  PWIO; //power contrl io
-        uint8_t  GAIN; // amplification factor
-        uint32_t OFFSET = 0; // used for tare weight
-        float    SCALE = 1; // used to return weight in grams, kg, ounces, whatever
+        uint8_t  sck; // Power Down and Serial Clock Input Pin
+        uint8_t  dout; // Serial Data Output Pin
+        uint8_t  pwio; //power contrl io
+        uint8_t  gain; // amplification factor
+        uint32_t offset_value = 0; // used for tare weight
+        float    scale_value = 1; // used to return weight in grams, kg, ounces, whatever
 
 };
 
